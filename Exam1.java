1e6ffc61820ad634280a3b48077af60374bf6694
sihyn <sihyn126@gmail.com>
2022-05-20 10:13:27 +0900

    Algorithm1

---
+++ Exam1.java
@@ -0,0 +1,24 @@
+package Exam;
+
+import java.util.Scanner;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		String[] str = new String[num];
+
+		for (int i = 0; i < num; i++) {
+			str[i] = scanner.next();
+		}
+
+		System.out.printf("Hello ");
+		for (int i = 0; i < num - 1; i++) {
+			System.out.printf(str[i]+",");
+		}
+		System.out.printf(str[num - 1]+".");
+
+	}
+}
